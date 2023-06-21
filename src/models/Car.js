800b948d30624eb345cb93d0f256541b86609970
William Edwards <williamedwards7717@gmail.com>
2023-06-20 23:47:35 -0600

    homepage

---
+++ src/models/Car.js
@@ -0,0 +1,9 @@
+export class Car {
+    constructor(data) {
+        this.id = data.id
+        this.make = data.make
+        this.model = data.model
+        this.price = data.price
+        this.img = data.img
+    }
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
+import { AppState } from "../AppState.js";
+import { router } from "../Router.jsx";
+import { Route } from "react-router-dom";
 
 export default function HomePage() {
   const [count, setCount] = useState(0)
+  const [car, createCar] = useState(Car)
 
   return (
     <div className="home-page">
       <div className="container my-3">
-        <div className="row">
-          <div className="col-4">
+        <div className="row justify-content-end">
+          <div className="col-4 text-end">
             <div className="card">
               <div className="card-body">
                 <button className="btn btn-success my-1" onClick={() => setCount((count) => count + 1)}>
                   count is {count}
                 </button>
+                <button className="btn btn-danger my-1" onClick={() => createCar((car) => )}>Create Car</button>
               </div>
             </div>
           </div>
